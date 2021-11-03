8901d9b1cf5f1e02be499adfa8953b2db31884bd
FriendlyKim <kimycmaster@gmail.com>
2021-11-03 09:29:36 +0900

    Main

---
+++ Assets/Scripts/Girl1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Girl1 : MonoBehaviour
+{
+    public Transform hairGroup;
+    public Transform upperGroup;
+    public Transform bodyGroup;
+
+    List<GameObject> hairs = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
